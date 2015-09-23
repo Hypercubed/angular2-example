83aaad75fb3bb353b21b5cda7ba7de0a83c311a5
J. Harshbarger <hypercubed@gmail.com>
2015-09-23 23:02:45 +0900

    Update 2015-09-23T14:02:19.652Z

--- src/boot.js
+++
@@ -1,9 +0,0 @@
-import 'zone.js';
-import 'reflect-metadata';
-
-import {bootstrap} from 'angular2/angular2';
-import {AppComponent} from './app';
-
-import 'bootstrap/css/bootstrap.css!';
-
-bootstrap(AppComponent);
